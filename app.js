62f9834ad256bcfcd5b605eb8aef49e2af62672a
Patricio <wearetamo@gmail.com>
2019-07-01 00:19:14 -0400

    Pure NodeJS writting message to .txt file

---
+++ app.js
@@ -0,0 +1,47 @@
+const HTTP = require('http');
+const FS = require('fs');
+
+const SERVER = HTTP.createServer((req, res) => {
+  const URL = req.url;
+  const METHOD = req.method;
+  res.setHeader('Content-Type', 'text/html');
+  if (URL === '/') {
+    res.write(
+      `<html>
+            <head><title>My first Page</title>
+            <body>
+                <form action="/message" method="POST">
+                    <input type="text" name="message"/>
+                    <button>SEND</button>
+                </form>
+            </body>
+        </html>`
+    );
+    return res.end();
+  }
+
+  if (URL === '/message' && METHOD === 'POST') {
+    const BODY = [];
+    req.on('data', chunk => {
+      BODY.push(chunk);
+    });
+    req.on('end', () => {
+      const PARSED_BODY = Buffer.concat(BODY).toString();
+      const MESSAGE = PARSED_BODY.split('=')[1];
+      FS.writeFileSync('message.txt', MESSAGE);
+    });
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
+    return res.end();
+  }
+  res.write(
+    `<html>
+          <head><title>My first Page</title>
+          <body>
+            YAY!
+          </body>
+      </html>`
+  );
+});
+
+SERVER.listen(3000);
