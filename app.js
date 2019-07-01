e44253bd08609fdf2d4ccbf5f4959dbb8825a830
Patricio <wearetamo@gmail.com>
2019-07-01 15:31:05 -0400

    added route shorcut

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const EXPRESS = require('express');
 const BODY_PARSER = require('body-parser');
+
 const APP = EXPRESS();
+
 const ADMIN_ROUTES = require('./routes/admin');
-const SHOPE_ROUTES = require('./routes/shop');
+const SHOP_ROUTES = require('./routes/shop');
 
 APP.use(BODY_PARSER.urlencoded({ extended: false }));
-APP.use(ADMIN_ROUTES);
-APP.use(SHOPE_ROUTES);
 
-APP.listen(3000);
+APP.use('/admin', ADMIN_ROUTES);
+APP.use(SHOP_ROUTES);
+
+APP.use((req, res, next) => {
+  console.log('page not found');
+  res.status(404).send('<h1>Page not Foudn</h1>');
+});
+
+APP.listen(4000);
