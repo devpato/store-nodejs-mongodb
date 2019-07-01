c83925a9588d81f413b3fb5abafdf358a1cff983
Patricio <wearetamo@gmail.com>
2019-07-01 00:34:28 -0400

    created route

---
+++ routes.js
@@ -0,0 +1,50 @@
+const FS = require('fs');
+
+const REQUEST_HANDLER = (req, res) => {
+  const URL = req.url;
+  const METHOD = req.method;
+  res.setHeader('Content-Type', 'text/html');
+  if (URL === '/') {
+    res.write(
+      `<html>
+          <head><title>My first Page</title>
+          <body>
+              <form action="/message" method="POST">
+                  <input type="text" name="message"/>
+                  <button>SEND</button>
+              </form>
+          </body>
+      </html>`
+    );
+    return res.end();
+  }
+
+  if (URL === '/message' && METHOD === 'POST') {
+    const BODY = [];
+    req.on('data', chunk => {
+      BODY.push(chunk);
+    });
+    return req.on('end', () => {
+      const PARSED_BODY = Buffer.concat(BODY).toString();
+      const MESSAGE = PARSED_BODY.split('=')[1];
+      FS.writeFile('message.txt', MESSAGE, error => {
+        if (!error) {
+          res.statusCode = 302;
+          res.setHeader('Location', '/');
+          return res.end();
+        }
+      });
+    });
+  }
+  res.write(
+    `<html>
+        <head><title>My first Page</title>
+        <body>
+          YAY!
+        </body>
+    </html>`
+  );
+  return res.end();
+};
+
+module.exports = REQUEST_HANDLER;
