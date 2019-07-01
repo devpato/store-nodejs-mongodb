b920b6c6198a7d5f3a8df52bf30def5cc4b874a7
Patricio <wearetamo@gmail.com>
2019-07-01 14:53:33 -0400

    body parser

--- app.js
+++ app.js
@@ -1,4 +1,28 @@
 const HTTP = require('http');
-const ROUTES = require('./routes');
-const SERVER = HTTP.createServer(ROUTES);
+const EXPRESS = require('express');
+const BODY_PARSER = require('body-parser');
+const APP = EXPRESS();
+
+APP.use(BODY_PARSER.urlencoded({ extended: false }));
+
+APP.use('/', (req, res, next) => {
+  res.send('<h1>Test</h1>');
+});
+
+APP.use('/add-product', (req, res, next) => {
+  res.send(
+    `
+    <form action="/product" method="POST">
+        <input type="text" name="title" />
+        <button type="submit">Add</button>
+    </form>
+    `
+  );
+});
+
+APP.post('/product', (req, res, next) => {
+  res.redirect('/');
+});
+
+const SERVER = HTTP.createServer(APP);
 SERVER.listen(3000);
