1fae6559614c0d2c6c71c9f215c7073f25628a67
Patricio <wearetamo@gmail.com>
2019-07-01 15:01:14 -0400

    handeling routes

--- app.js
+++ app.js
@@ -1,28 +1,11 @@
-const HTTP = require('http');
 const EXPRESS = require('express');
 const BODY_PARSER = require('body-parser');
 const APP = EXPRESS();
+const ADMIN_ROUTES = require('./routes/admin');
+const SHOPE_ROUTES = require('./routes/shop');
 
 APP.use(BODY_PARSER.urlencoded({ extended: false }));
+APP.use(ADMIN_ROUTES);
+APP.use(SHOPE_ROUTES);
 
-APP.use('/', (req, res, next) => {
-  res.send('<h1>Test</h1>');
-});
-
-APP.use('/add-product', (req, res, next) => {
-  res.send(
-    `
-    <form action="/product" method="POST">
-        <input type="text" name="title" />
-        <button type="submit">Add</button>
-    </form>
-    `
-  );
-});
-
-APP.post('/product', (req, res, next) => {
-  res.redirect('/');
-});
-
-const SERVER = HTTP.createServer(APP);
-SERVER.listen(3000);
+APP.listen(3000);
