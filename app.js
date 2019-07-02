316585e85937bead77f3ad1539af0053847f357b
Patricio <wearetamo@gmail.com>
2019-07-02 14:21:21 -0400

    using ejs template

--- app.js
+++ app.js
@@ -5,17 +5,18 @@ const bodyParser = require('body-parser');
 const expressHbs = require('express-handlebars');
 
 const app = express();
-
-app.engine(
-  'hbs',
-  expressHbs({
-    layoutsDir: 'views/layouts/',
-    defaultLayout: 'main-layout',
-    extname: 'hbs'
-  })
-);
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
+
+// app.engine(
+//   'hbs',
+//   expressHbs({
+//     layoutsDir: 'views/layouts/',
+//     defaultLayout: 'main-layout',
+//     extname: 'hbs'
+//   })
+// );
+//app.set('view engine', 'hbs');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
