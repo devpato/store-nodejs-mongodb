99fc3f7b43cd60685b8c606aedf399e808c79e3d
Patricio <wearetamo@gmail.com>
2019-07-03 13:30:11 -0400

    using partials

--- routes/shop.js
+++ routes/shop.js
@@ -7,6 +7,7 @@ router.get('/', shopController.getIndex);
 router.get('/products', shopController.getProducts);
 router.get('/products/:productId', shopController.getProduct);
 router.get('/cart', shopController.getCart);
+router.get('/cart', shopController.postCart);
 router.get('/orders', shopController.getOrders);
 router.get('/checkout', shopController.getCheckout);
 
