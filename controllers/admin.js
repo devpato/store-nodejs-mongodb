b57603df8557d6099b9830fdb5d5c564da0ca355
Patricio <wearetamo@gmail.com>
2019-07-03 14:28:14 -0400

    edit product controller

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,7 +1,7 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-  res.render('admin/add-product', {
+  res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
     formsCSS: true,
@@ -18,6 +18,23 @@ exports.postAddProduct = (req, res, next) => {
   const product = new Product(title, imageUrl, description, price);
   product.save();
   res.redirect('/');
+};
+
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit;
+
+  if (!editMode) {
+    return res.redirect('/');
+  }
+
+  res.render('admin/edit-product', {
+    pageTitle: 'Edit Product',
+    path: '/admin/edit-product',
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true,
+    editing: editMode
+  });
 };
 
 exports.getProducts = (req, res, next) => {
