e9997a159e804c39f1b0ea1f63beb429dd13d91e
Patricio <wearetamo@gmail.com>
2019-07-04 14:27:34 -0400

    creting a table based on Product model

--- app.js
+++ app.js
@@ -3,18 +3,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const errorController = require('./controllers/error.js');
 const app = express();
+const sequelize = require('./util/db');
+
 app.set('view engine', 'ejs');
 app.set('views', 'views');
-
-// app.engine(
-//   'hbs',
-//   expressHbs({
-//     layoutsDir: 'views/layouts/',
-//     defaultLayout: 'main-layout',
-//     extname: 'hbs'
-//   })
-// );
-//app.set('view engine', 'hbs');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -26,5 +18,14 @@ app.use('/admin', adminData);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
+
+sequelize
+  .sync()
+  .then(results => {
+    console.log(results);
+  })
+  .catch(err => {
+    console.log(err);
+  });
 
 app.listen(3000);
