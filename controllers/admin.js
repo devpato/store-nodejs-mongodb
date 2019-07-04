cd9ca2a34771911b7835b84b303403178bb492ce
Patricio <wearetamo@gmail.com>
2019-07-04 18:01:26 -0400

    creating a product with the user

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,12 +13,16 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  Product.create({
-    title: title,
-    price: price,
-    imageUrl: imageUrl,
-    description: description
-  })
+
+  //Creates a product and adds the user by default
+  req.user
+    .createProduct({
+      title: title,
+      price: price,
+      imageUrl: imageUrl,
+      description: description,
+      userId: req.user.id
+    })
     .then(results => {
       console.log('Created Product');
       res.redirect('/admin/products');
