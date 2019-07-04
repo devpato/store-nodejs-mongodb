d76b59c277e5ddca5666793868c13690f9989af2
Patricio <wearetamo@gmail.com>
2019-07-04 14:22:40 -0400

    created product model que squalizer

--- models/product.js
+++ models/product.js
@@ -1,28 +1,26 @@
-const Cart = require('../models/cart');
-const db = require('../util/db');
-module.exports = class Product {
-  constructor(id, title, imageUrl, description, price) {
-    this.id = id;
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = price;
-  }
+const Sequelize = require('sequelize');
+const sequelize = require('../util/db');
 
-  save() {
-    return db.execute(
-      'INSERT INTO products (title, price, description,imageUrl) VALUES (?, ?, ?, ?)',
-      [this.title, this.price, this.description, this.imageUrl]
-    );
+const Product = sequelize.define('product', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  title: Sequelize.STRING,
+  price: {
+    type: Sequelize.DOUBLE,
+    allowNull: false
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false
   }
+});
 
-  static fetchAll() {
-    return db.execute('SELECT * FROM products');
-  }
-
-  static findById(id) {
-    return db.execute('SELECT * FROM products WHERE products.id = ?', [id]);
-  }
-
-  static deleteById(id) {}
-};
+module.exports = Product;
