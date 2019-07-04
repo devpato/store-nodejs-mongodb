6a2ce26c35b7992d9dfa4a49f27a9525732ab0e4
Patricio <wearetamo@gmail.com>
2019-07-04 15:06:17 -0400

    fixed bug

--- controllers/shop.js
+++ controllers/shop.js
@@ -17,17 +17,33 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId)
-    .then(([product]) => {
-      res.render('shop/product-detail', {
-        product: product[0],
-        pageTitle: product.title,
-        path: '/products'
-      });
-    })
-    .catch(err => {
-      console.log(err);
+  console.log('product id');
+  console.log(prodId);
+
+  Product.findAll({
+    where: {
+      id: prodId
+    }
+  }).then(products => {
+    console.log('************');
+    console.log(products[0]);
+    res.render('shop/product-detail', {
+      product: products[0],
+      pageTitle: '',
+      path: '/products'
     });
+  });
+  // Product.findByPk(prodId)
+  //   .then(product => {
+  //     res.render('shop/product-detail', {
+  //       product: product,
+  //       pageTitle: product.title,
+  //       path: '/products'
+  //     });
+  //   })
+  //   .catch(err => {
+  //     console.log(err);
+  //   });
 };
 
 exports.getIndex = (req, res, next) => {
