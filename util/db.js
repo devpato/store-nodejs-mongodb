ffd5c5f29e962ae5ecb6c7759ec07089070adedc
Patricio <wearetamo@gmail.com>
2019-07-04 19:07:36 -0400

    deteled file

--- util/db.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize('node-store', 'root', 'Policia9@', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
-
-module.exports = sequelize;
