8a7d3c8b45a7451703d77b47effb98846a2a570d
Patricio <wearetamo@gmail.com>
2019-07-04 14:27:16 -0400

    db config using sequilize

--- util/db.js
+++ util/db.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('node-store', 'root', 'nodestore', {
-  dialectL: 'mysql',
+const sequelize = new Sequelize('node-store', 'root', 'Policia9@', {
+  dialect: 'mysql',
   host: 'localhost'
 });
 
