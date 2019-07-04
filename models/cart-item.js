085fa607d72dfd91ef11dfdd208ac0193c7817b3
Patricio <wearetamo@gmail.com>
2019-07-04 19:07:29 -0400

    add cart item

--- models/cart-item.js
+++ models/cart-item.js
@@ -1,7 +1,8 @@
 const Sequelize = require('sequelize');
-const sequelize = require('../util/db');
 
-const CartItem = sequelize.define('cart', {
+const sequelize = require('../util/database');
+
+const CartItem = sequelize.define('cartItem', {
   id: {
     type: Sequelize.INTEGER,
     autoIncrement: true,
