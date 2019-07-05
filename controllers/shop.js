3bd9108945c1af5ecb247674aa9086ace96ba864
Patricio <wearetamo@gmail.com>
2019-07-05 17:51:52 -0400

    commented code

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,17 +1,17 @@
 const Product = require('../models/product');
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
-    .then(products => {
-      res.render('shop/product-list', {
-        prods: products,
-        pageTitle: 'All Products',
-        path: '/products'
-      });
-    })
-    .catch(err => {
-      console.log(err);
-    });
+  // Product.findAll()
+  //   .then(products => {
+  //     res.render('shop/product-list', {
+  //       prods: products,
+  //       pageTitle: 'All Products',
+  //       path: '/products'
+  //     });
+  //   })
+  //   .catch(err => {
+  //     console.log(err);
+  //   });
 };
 
 exports.getProduct = (req, res, next) => {
