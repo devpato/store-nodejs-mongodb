dc2c10618c1cae07e0afbeb84ad81266b3fb4795
Patricio <wearetamo@gmail.com>
2019-07-05 18:06:16 -0400

    get index, products and product

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,31 +1,22 @@
 const Product = require('../models/product');
 
 exports.getProducts = (req, res, next) => {
-  // Product.findAll()
-  //   .then(products => {
-  //     res.render('shop/product-list', {
-  //       prods: products,
-  //       pageTitle: 'All Products',
-  //       path: '/products'
-  //     });
-  //   })
-  //   .catch(err => {
-  //     console.log(err);
-  //   });
+  Product.find()
+    .then(products => {
+      res.render('shop/product-list', {
+        prods: products,
+        pageTitle: 'All Products',
+        path: '/products'
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    });
 };
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  // Product.findAll({ where: { id: prodId } })
-  //   .then(products => {
-  //     res.render('shop/product-detail', {
-  //       product: products[0],
-  //       pageTitle: products[0].title,
-  //       path: '/products'
-  //     });
-  //   })
-  //   .catch(err => console.log(err));
-  Product.findByPk(prodId)
+  Product.findById(prodId)
     .then(product => {
       res.render('shop/product-detail', {
         product: product,
@@ -37,7 +28,7 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.findAll()
+  Product.find()
     .then(products => {
       res.render('shop/index', {
         prods: products,
