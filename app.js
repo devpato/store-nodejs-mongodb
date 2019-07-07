b0d9c52a77a58b16f36b113e5ad0149c84ef394a
Patricio <wearetamo@gmail.com>
2019-07-07 01:14:21 -0400

    using dynamic user

--- app.js
+++ app.js
@@ -32,7 +32,11 @@ app.use(
 );
 
 app.use((req, res, next) => {
-  User.findById('5d1fcc9b0ae04918c4e64479')
+  if (!req.session.user) {
+    return next();
+  }
+
+  User.findById(req.session.user._id)
     .then(user => {
       req.user = user;
       next();
