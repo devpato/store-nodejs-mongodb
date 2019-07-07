407fb1735da7e5ec3c5846d11ad05034cd460875
Patricio <wearetamo@gmail.com>
2019-07-07 01:38:25 -0400

    adding session

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,33 +1,26 @@
-const User = require('../models/user.js');
+const User = require('../models/user');
+
 exports.getLogin = (req, res, next) => {
-  // const isLoggedIn =
-  //   req
-  //     .get('Cookie')
-  //     .split(';')
-  //     .pop()
-  //     .trim()
-  //     .split('=')[1] === 'true';
-  console.log(req.session.isLoggedIn);
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
-    isAuthenticated: req.session.isLoggedIn
+    isAuthenticated: false
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  User.findById('5d1fcc9b0ae04918c4e64479')
+  User.findById('5d2182d640e86a3bbe8f6925')
     .then(user => {
       req.session.isLoggedIn = true;
       req.session.user = user;
-      return req.session.save();
+      req.session.save(err => {
+        console.log(err);
+
+        res.redirect('/');
+      });
     })
-    .then(() => {
-      res.redirect('/');
-    })
-    .catch(err => {
-      console.log(err);
-    });
+
+    .catch(err => console.log(err));
 };
 
 exports.postLogout = (req, res, next) => {
