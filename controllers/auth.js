b8678d710af4352c126fd793d9e265ff1f22b1a6
Patricio <wearetamo@gmail.com>
2019-07-07 00:25:51 -0400

    using session in the auth contoller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,20 @@
 exports.getLogin = (req, res, next) => {
+  // const isLoggedIn =
+  //   req
+  //     .get('Cookie')
+  //     .split(';')
+  //     .pop()
+  //     .trim()
+  //     .split('=')[1] === 'true';
+  console.log(isLoggedIn);
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
-    isAuthenticated: req.isLoggedIn
+    isAuthenticated: res.session.isLoggedIn
   });
 };
 
 exports.postLogin = (req, res, next) => {
-    req.isLoggedIn = true;
-    res.redirect('/');
-  };
+  res.session.isLoggedIn = true;
+  res.redirect('/');
+};
