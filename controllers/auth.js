762c3c4f3714b47189894a015e3ba41f054dc749
Patricio <wearetamo@gmail.com>
2019-07-08 02:22:52 -0400

    added crypto to generate tokens

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcryptjs');
 const nodemailer = require('nodemailer');
 const sendGridTransport = require('nodemailer-sendgrid-transport');
+const crypto = require('crypto');
 const User = require('../models/user');
 
 const transporter = nodemailer.createTransport(
@@ -129,5 +130,42 @@ exports.getResetPassword = (req, res, next) => {
     path: '/reset',
     pageTitle: 'Reset Password',
     errorMessage: message
+  });
+};
+
+exports.postResetPassword = (req, res, next) => {
+  crypto.randomBytes(32, (err, buffer) => {
+    if (err) {
+      console.log(err);
+      return res.redirect('/reset');
+    }
+
+    const token = buffer.toString('hex');
+    User.findOne({ email: req.body.email })
+      .then(user => {
+        if (!user) {
+          req.flash('Not account with that email found!');
+          return res.redirect('/reset');
+        }
+        user.resetToken = token;
+        user.resetTokenExpiration = Date.now() + 3600000;
+        return user.save();
+      })
+      .then(() => {
+        console.log('User with token saved correctly !');
+        res.redirect('/');
+        transporter.sendMail({
+          to: req.body.email,
+          from: 'shop@nodejs.com',
+          subject: 'Password Reset from O-Store',
+          html: `
+          <p>Your password reset link</p>
+          <p>Click this <a href="http://localhost:3000/reset/${token}">link</a> to set a new password</p>
+          `
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
   });
 };
