9612e5506fa7daf8f6ee9d144d501c2d65d0ea30
Patricio <wearetamo@gmail.com>
2019-07-13 19:14:04 -0400

    check validator

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const { check, body } = require('express-validator/check');
 const authController = require('../controllers/auth');
 
 const router = express.Router();
@@ -10,7 +10,22 @@ router.get('/signup', authController.getSignup);
 
 router.post('/login', authController.postLogin);
 
-router.post('/signup', authController.postSignup);
+router.post(
+  '/signup',
+  check('email')
+    .isEmail()
+    .withMessage('Please enter a valid email')
+    .custom((value, { req }) => {
+      if (value === 'test@test.com') {
+        throw new Error('Test email is forbidden');
+      }
+      return true;
+    }),
+  body('password', 'Please enter a stronger password')
+    .isLength({ min: 5 })
+    .isAlphanumeric(),
+  authController.postSignup
+);
 
 router.post('/logout', authController.postLogout);
 
