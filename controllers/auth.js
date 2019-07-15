5797214b218f137db2be73113126c36041df7355
Patricio <wearetamo@gmail.com>
2019-07-14 20:14:55 -0400

    added throw erros

--- controllers/auth.js
+++ controllers/auth.js
@@ -114,7 +114,11 @@ exports.postLogin = (req, res, next) => {
           res.redirect('/login');
         });
     })
-    .catch(err => console.log(err));
+    .catch(err => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(err);
+    });
 };
 
 exports.postSignup = (req, res, next) => {
@@ -157,7 +161,9 @@ exports.postSignup = (req, res, next) => {
       // });
     })
     .catch(err => {
-      console.log(err);
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(err);
     });
 };
 
@@ -212,7 +218,9 @@ exports.postReset = (req, res, next) => {
         });
       })
       .catch(err => {
-        console.log(err);
+        const error = new Error(err);
+        error.httpStatusCode = 500;
+        return next(err);
       });
   });
 };
@@ -236,7 +244,9 @@ exports.getNewPassword = (req, res, next) => {
       });
     })
     .catch(err => {
-      console.log(err);
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(err);
     });
 };
 
@@ -264,7 +274,9 @@ exports.postNewPassword = (req, res, next) => {
     .then(result => {
       res.redirect('/login');
     })
-    .catch(err => {
-      console.log(err);
+   .catch(err => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(err);
     });
 };
